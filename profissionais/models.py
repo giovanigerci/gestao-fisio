1d7f1836c6cefa8b21eb41da9bf5f0fe98f07681
Giovani Gerci <132081746+giovanigerci@users.noreply.github.com>
2026-07-16 20:23:19 -0300

    feat: cria apps profissionais e clinicas com models e migrations iniciais

---
+++ profissionais/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profissional(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefone = models.CharField(max_length=20)
+    especialidade = models.CharField(max_length=100)
+    crefito = models.CharField(max_length=15, unique=True)
+
+
+    def __str__(self):
+        return self.usuario.get_full_name() or self.usuario.username
